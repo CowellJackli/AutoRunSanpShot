4a4971cecd51bd94e4d593f09b7568f0d32243fb
CowellJackli <jackli@cowell.com.tw>
2015-07-07 20:40:04 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoRunSnapShot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan TsStart = new TimeSpan(DateTime.Now.Ticks);
+            String[] CmdArgs= System.Environment.GetCommandLineArgs();
+            if (CmdArgs.Length > 1)
+            {
+                //参数0是它本身的路径
+                String FileName = CmdArgs[1].ToString();
+                String Dir = CmdArgs[2].ToString();
+                String Url = CmdArgs[3].ToString();
+                SnapShot sp = new SnapShot();
+                sp.FileName = FileName;// "test.jpg";
+                sp.Dir = Dir; //@"D:\web\7.03\eweb_lifetour";
+                sp.Url = Url; //@"http://lifetour20.travelindex.com.tw/eWeb/GO/V_GO_Detail.asp?MGRUP_CD=0000&GRUP_CD=0000141015A&SUB_CD=GO&JOIN_TP=1";
+                sp.Save();
+                
+                
+            }
+            
+            System.Timers.Timer tmr = new System.Timers.Timer(20000);
+            tmr.Elapsed += delegate
+            {
+                Environment.Exit(0);
+            };
+            tmr.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnapShot.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoRunSnapShot
+{
+
+    class SnapShot
+    {
+        public string Url { get; set; }
+        public string FileName { get; set; }
+        public string Dir { get; set; }
+       
+
+        internal void Save()
+        {
+
+            StartBrowser(Url);
+
+
+        }
+
+        private void StartBrowser(string url)
+        {
+            var th = new Thread(() =>
+             {
+
+                 var webBrowser = new WebBrowser();
+                 webBrowser.ScrollBarsEnabled = false;
+                 webBrowser.DocumentCompleted +=
+                     webBrowser_DocumentCompleted;
+                 webBrowser.Width = 960;
+                 webBrowser.Height = 2500;
+                 webBrowser.ScriptErrorsSuppressed = true;
+                 webBrowser.Url = new Uri(Url);
+                 Application.Run();
+                 Console.WriteLine("開始執行");
+             });
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+           
+        }
+
+        private void  webBrowser_DocumentCompleted(object sender,WebBrowserDocumentCompletedEventArgs e)
+        {
+            var webBrowser = (WebBrowser)sender;
+            webBrowser.Width = (webBrowser.Document.Body.ScrollRectangle.Size.Width == 0) ? webBrowser.Width : webBrowser.Document.Body.ScrollRectangle.Size.Width;
+            webBrowser.Height = (webBrowser.Document.Body.ScrollRectangle.Size.Height == 0) ? webBrowser.Height : webBrowser.Document.Body.ScrollRectangle.Size.Height;
+
+            using (Bitmap bitmap = new Bitmap(
+                    webBrowser.Width,
+                    webBrowser.Height))
+            {
+                webBrowser
+                    .DrawToBitmap(
+                    bitmap,
+                    new System.Drawing
+                        .Rectangle(0, 0, bitmap.Width, bitmap.Height));
+                bitmap.Save(Dir + @"\" + FileName,
+                    System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+    }
+}
